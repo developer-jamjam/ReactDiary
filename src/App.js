6a0d537dd3279046841e58399fb51dc46801efff
404_jamjam <jhy1025a@gmail.com>
2023-03-05 20:26:43 +0900

    react_mini_project_9
    React 상태관리 로직 분리
    useReducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState, useRef, useEffect, useMemo,useCallback} from "react";
+import React,{useRef, useEffect, useMemo,useCallback, useReducer} from "react";
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
@@ -43,8 +43,38 @@ import DiaryList from './DiaryList';
 //     create_date: new Date().getTime()
 //   },
 // ]
+const reducer = (state, action) =>{
+  switch(action.type) {
+    case 'INIT' : {
+      return action.data
+    }
+    case 'CREATE' : {
+      const create_date = new Date().getTime();
+      const newItem ={
+        ...action.data,
+        create_date
+      }
+      return [newItem, ...state]
+    }
+
+    case 'REMOVE' : {
+      return state.filter((it)=>it.id !== action.targetId);
+    }
+    case 'EDIT' :{
+      return state.map((it)=>
+        it.id === action.targetId ? 
+        {...it, content:action.newContent} : it
+      );
+    }
+
+    default :
+      return state;
+  }
+}
+
 const App =() => {
-  const [data,setData] = useState([]);
+ // const [data,setData] = useState([]);
+  const [data,dispatch] = useReducer(reducer,[]);
 
   const dataId = useRef(0);
 
@@ -62,8 +92,8 @@ const App =() => {
         id : dataId.current++,
       };
     });
-
-    setData(initData);
+    
+    dispatch({type:"INIT", data:initData})
   } //API 호출
 
   useEffect(()=>{
@@ -73,28 +103,19 @@ const App =() => {
   },[]);
 
   const onCreate = useCallback((author,content,emotion) => {
-    const create_date = new Date().getTime();
-    const newItem = {
-      author,
-      content,
-      emotion,
-      create_date,
-      id: dataId.current
-    }
+    dispatch({
+      type:"CREATE",
+      data:{author,content,emotion,id:dataId.current}
+    });
     dataId.current += 1;
-    setData((data)=>[newItem, ...data]);
   }, []);
 
   const onRemove = useCallback((targetId) => {
-    setData(data => data.filter((it) => it.id !== targetId));
+    dispatch({type:"REMOVE", targetId})
   },[]);
 
   const onEdit = useCallback((targetId, newContent) => {
-    setData((data) =>
-      data.map((it)=>
-        it.id === targetId ? {...it, content:newContent} : it
-      )
-    );
+    dispatch({ type:"EDIT", targetId,newContent });
   },[]);
 
   const getDiaryAnalysis = useMemo(
